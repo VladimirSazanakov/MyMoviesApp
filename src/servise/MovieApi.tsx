82f2cbe44e37913b966bdee12327a1160a6b2487
Vladimir <sazanakov@inbox.ru>
2023-08-01 00:34:53 +0700

    create api

---
+++ src/servise/MovieApi.tsx
@@ -0,0 +1,33 @@
+export default class MovieApi {
+  _apiBase = 'https://api.themoviedb.org/3/search/movie';
+
+  options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODAyOGU0ZThlMzk3MzA1YTVlOGY4Zjc3NzA3ZTYzZSIsInN1YiI6IjY0YzdkYzcyZWVjNWI1NThmMDNiZjM2MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1ZUQtYugyYmsM3ZBrpygsZw10HaStCmJXuyboy2aV04'
+    }
+  };
+
+  async getResounse(url : string){
+    console.log(`${this._apiBase}${url}`);
+    const res = await fetch(`${this._apiBase}${url}`, this.options)
+
+    if(!res.ok){
+      throw new Error(`Error to get monies api ${url}`)
+    }
+    return res.json()
+  }
+
+  async getAllMovies(params: string) {
+    const res = await this.getResounse(`?query=${params}`);
+    return res.results;
+  }
+  /*
+  fetch('https://api.themoviedb.org/3/search/movie?query=return', options)
+    .then(response => response.json())
+    .then(response => response.results)
+    .then(result => setMovies(result))
+    .catch(err => console.error(err));
+*/
+}
