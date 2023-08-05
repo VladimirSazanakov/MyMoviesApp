9bc63d132046c5a2fad350beaeef9a7ef649a803
VladimirSazanakov <sazanakov@mail.ru>
2023-08-05 20:42:18 +0700

    2.3.14 alpha2

---
+++ src/components/SearchPage/RatePage/RatePage.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+import './RatePge.css';
+
+export default function RatePage() {
+  return (
+    <p>Rate Page Test</p>
+  )
+}
---
+++ src/components/SearchPage/SearchPage.tsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+import './SearchPge.css';
+
+export default function SearchPage() {
+  return (
+    <p>Search Page Test</p>
+  )
+}
